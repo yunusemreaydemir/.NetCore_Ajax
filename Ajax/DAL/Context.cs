beceacaad231adca5cd6180a0f91ec743254bda0
Yunus Emre Aydemir <yunusemre.aydemir@outlook.com>
2023-08-03 09:32:25 +0300

    AjaxProject

---
+++ Ajax/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ajax.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-FIKBBE9;initial catalog = AjaxDb; integrated Security=true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
